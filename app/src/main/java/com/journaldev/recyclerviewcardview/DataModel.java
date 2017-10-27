5abb489ca615de96f56f2aa11210f3c9730dd905
daniyalahmedkhan <daniyalahmed520@gmail.com>
2017-10-27 15:08:12 +0500

    Initial commit

---
+++ app/src/main/java/com/journaldev/recyclerviewcardview/DataModel.java
@@ -0,0 +1,31 @@
+package com.journaldev.recyclerviewcardview;
+
+
+public class DataModel {
+
+
+    String name;
+    String version;
+    int id_;
+    int image;
+
+    public DataModel(String name, int id_, int image) {
+        this.name = name;
+        this.id_ = id_;
+        this.image=image;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getImage() {
+        return image;
+    }
+
+    public int getId() {
+        return id_;
+    }
+}
---
+++ app/src/main/java/com/journaldev/recyclerviewcardview/MyData.java
@@ -0,0 +1,14 @@
+package com.journaldev.recyclerviewcardview;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo"};
+//    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
+
+    static Integer[] drawableArray = {R.drawable.home_categories_tab_one, R.drawable.home_categories_tab_two, R.drawable.home_categories_tab_one,
+            R.drawable.home_categories_tab_two};
+
+    static Integer[] id_ = {0, 1, 2, 3};
+}
