f3de1c512fd8a09ed71c195b99879089bbee4823
daniyalahmedkhan <daniyalahmed520@gmail.com>
2017-10-27 15:39:15 +0500

    COMMENT CODE FOR REMOVE AND ADD

--- app/src/main/java/com/journaldev/recyclerviewcardview/MainActivity.java
+++ app/src/main/java/com/journaldev/recyclerviewcardview/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myOnClickListener = new MyOnClickListener(this);
+//        myOnClickListener = new MyOnClickListener(this);
 
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -55,67 +55,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private static class MyOnClickListener implements View.OnClickListener {
+//    private static class MyOnClickListener implements View.OnClickListener {
+//
+//        private final Context context;
+//
+//        private MyOnClickListener(Context context) {
+//            this.context = context;
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            removeItem(v);
+//        }
+//
+//        private void removeItem(View v) {
+//            int selectedItemPosition = recyclerView.getChildPosition(v);
+//            RecyclerView.ViewHolder viewHolder
+//                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+//            TextView textViewName
+//                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+//            String selectedName = (String) textViewName.getText();
+//            int selectedItemId = -1;
+//            for (int i = 0; i < MyData.nameArray.length; i++) {
+//                if (selectedName.equals(MyData.nameArray[i])) {
+//                    selectedItemId = MyData.id_[i];
+//                }
+//            }
+//            removedItems.add(selectedItemId);
+//            data.remove(selectedItemPosition);
+//            adapter.notifyItemRemoved(selectedItemPosition);
+//        }
 
-        private final Context context;
 
-        private MyOnClickListener(Context context) {
-            this.context = context;
-        }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
-        @Override
-        public void onClick(View v) {
-            removeItem(v);
-        }
+    //    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        super.onOptionsItemSelected(item);
+//        if (item.getItemId() == R.id.add_item) {
+//           //check if any items to add
+//            if (removedItems.size() != 0) {
+//                addRemovedItemToList();
+//            } else {
+//                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        return true;
+//    }
 
-        private void removeItem(View v) {
-            int selectedItemPosition = recyclerView.getChildPosition(v);
-            RecyclerView.ViewHolder viewHolder
-                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
-            TextView textViewName
-                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
-            String selectedName = (String) textViewName.getText();
-            int selectedItemId = -1;
-            for (int i = 0; i < MyData.nameArray.length; i++) {
-                if (selectedName.equals(MyData.nameArray[i])) {
-                    selectedItemId = MyData.id_[i];
-                }
-            }
-            removedItems.add(selectedItemId);
-            data.remove(selectedItemPosition);
-            adapter.notifyItemRemoved(selectedItemPosition);
-        }
+//    private void addRemovedItemToList() {
+//        int addItemAtListPosition = 3;
+//        data.add(addItemAtListPosition, new DataModel(
+//                MyData.nameArray[removedItems.get(0)],
+//                MyData.id_[removedItems.get(0)],
+//                MyData.drawableArray[removedItems.get(0)]
+//        ));
+//        adapter.notifyItemInserted(addItemAtListPosition);
+//        removedItems.remove(0);
+//    }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.add_item) {
-           //check if any items to add
-            if (removedItems.size() != 0) {
-                addRemovedItemToList();
-            } else {
-                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
-            }
-        }
-        return true;
-    }
-
-    private void addRemovedItemToList() {
-        int addItemAtListPosition = 3;
-        data.add(addItemAtListPosition, new DataModel(
-                MyData.nameArray[removedItems.get(0)],
-                MyData.id_[removedItems.get(0)],
-                MyData.drawableArray[removedItems.get(0)]
-        ));
-        adapter.notifyItemInserted(addItemAtListPosition);
-        removedItems.remove(0);
-    }
-}
--- app/src/main/java/com/journaldev/recyclerviewcardview/MyData.java
+++ app/src/main/java/com/journaldev/recyclerviewcardview/MyData.java
@@ -4,11 +4,11 @@ package com.journaldev.recyclerviewcardview;
 
 public class MyData {
 
-    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo"};
+    static String[] nameArray = {"MEN", "WOMEN", "TEEN", "KIDS"};
 //    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
 
-    static Integer[] drawableArray = {R.drawable.home_categories_tab_one, R.drawable.home_categories_tab_two, R.drawable.home_categories_tab_one,
-            R.drawable.home_categories_tab_two};
+    static Integer[] drawableArray = {R.mipmap.home_categories_tab_one, R.mipmap.home_categories_tab_two, R.mipmap.home_categories_tab_one,
+            R.mipmap.home_categories_tab_two};
 
     static Integer[] id_ = {0, 1, 2, 3};
 }
